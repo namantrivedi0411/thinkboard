0109f232f13d489804fc58fc8db2a8a25ac9378b
Naman Trivedi <namantrivedi0411@gmail.com>
2026-06-09 19:35:00 +0530

    Deployment Ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,28 +5,40 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
-
+import path from "path";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 //middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
+
+
 app.use(express.json());
 app.use(rateLimiter);
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 // app.use((req, res, next) => {
 //     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //     next();
 // });
 
-app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT:", PORT);
